3487c41e79318ea8218c55e093d410b4369e6b3b
Leotomas <marycolageo@gmail.com>
2020-05-01 08:53:42 -0400

    made changes to streamcheck to improve embed and seperate grid and other streamers

--- streamcheck.js
+++ streamcheck.js
@@ -2,14 +2,20 @@ const announceRole = "245272223101747200";
 var streamerAnnounceChannel = null;
 var gridServer = null;
 const fetch = require("node-fetch");
+const fs = require("fs");
 require('dotenv').config({path: "./.env"})
-var liveData = {
-}
+var liveData = {}
 
 module.exports = {
     async init(client) {
         streamerAnnounceChannel = client.channels.cache.get("464335012595105792");
         gridServer = client.guilds.cache.get("108417593513127936");
+        try {
+            liveData = JSON.parse(fs.readFileSync("./livedata.json"))
+        } catch (err) {
+            liveData = {}
+        }
+        
     },
     async twitch_fetch(url, method) {
         if (!method) method = "GET"
@@ -30,11 +36,11 @@ module.exports = {
             if (!data) return null;
             else {
                 const { user_name, title, game_id } = data;
-                //const game_name = this.get_game_name(game_id);
+                const game_name = this.get_game_name(game_id);
                 return {
                     user: user_name,
                     stream_title: title,
-                    //game: game_name
+                    game: game_name
                 }
             }
         }).catch(err => {
@@ -47,44 +53,71 @@ module.exports = {
         gridServer.members.cache.forEach(async m => {
             if (m.id == gridServer.owner.id || m.roles.cache.some(r => r.id == announceRole)) {
                 if (!liveData[m.id]) {
-                    liveData[m.id] = {twitch: undefined, islive: false}
+                    liveData[m.id] = {twitch: undefined, islive: false, liveMsgID: null}
                 }
-                let ud = liveData[m.id];
                 
-                if (!ud.twitch) {
+                if (!liveData[m.id].twitch) {
                     let activity = m.presence.activities.filter(m => m.type === "STREAMING")[0];
                     if (activity) {
-                        ud.twitch = activity.url.split(".tv/", 2)[1]
+                        liveData[m.id].twitch = activity.url.split(".tv/", 2)[1]
                         
                     }
                 }
                 
-                if (ud.twitch) {
+                if (liveData[m.id].twitch) {
                     let streaming = this.twitch(ud.twitch);
                     if (streaming !== null) {
-                        if (!ud.islive) {
+                        if (!liveData[m.id].islive) {
                             var discordProfilePicture = m.user.avatarURL();
-                            await streamerAnnounceChannel.send({embed:{
-                                color: 0x6441a5,
-                                title: `${m.displayName} just went live on twitch!`,
-                                url: "https://twitch.tv/" + ud.twitch,
-                                description: streaming.title,
-                                thumbnail: {
-                                    url: discordProfilePicture
-                                }
-                                }
-                            });
+                            if (m.id == gridServer.owner.id) {
+                                //TODO: replace streamerAnnounceChannel with twitch-announcements and affiliated-streamers
+                                await streamerAnnounceChannel.send({content: "<@&705490467063726090>", embed:{
+                                    color: 0x6441a5,
+                                    title: "Grid just went live on twitch!",
+                                    url: "https://twitch.tv/grid21",
+                                    description: userData["activities"][twitchIndex]["details"],
+                                    thumbnail: {
+                                        url: discordProfilePicture
+                                    },
+                                    image: {
+                                        url: "https://cdn.discordapp.com/attachments/703416034803056680/703416283596849242/Grid_Black_1080.png"
+                                    }
+                                }});
+                            } else {
+                                await streamerAnnounceChannel.send({embed:{
+                                    color: 0x6441a5,
+                                    title: `${m.displayName} just went live on twitch!`,
+                                    url: "https://twitch.tv/" + liveData[m.id].twitch,
+                                    thumbnail: {
+                                        url: discordProfilePicture
+                                    },
+                                    fields: [
+                                        {
+                                            name: "Title",
+                                            value: streaming.title,
+                                            inline: true
+                                        },
+                                        {
+                                            name: "Game",
+                                            value: streaming.game,
+                                            inline: true
+                                        }
+                                    ]
+                                }});
+                            }
+                            
                         }
-                        ud.islive = true;
+                        liveData[m.id].islive = true;
                     } else {
-                        if (ud.islive) {
+                        if (liveData[m.id].islive) {
                             // delete live message
                         }
-                        ud.islive = false;
+                        liveData[m.id].islive = false;
                     }
                 }
                 //console.log(ud)
             }
         });
+        fs.writeFileSync("./livedata.json", liveData);
     },
 };
