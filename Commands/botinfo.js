a5389d72cd6d4c7d2cd1942ee9169194ad1ac1c0
Grid21 <minilife92@gmail.com>
2020-05-02 14:40:13 -0400

    made changes to Messages, Fix Swear word filter.

--- Commands/botinfo.js
+++ Commands/botinfo.js
@@ -9,26 +9,27 @@ module.exports = {
      * @param {Array} args 
      */
     async run(msg, args) {
-       if (msg.member.roles.cache.has("160221051983233024") || msg.member.roles.cache.has("593667878063046666")) { //wait wait
-       //doesn't support an array in v12
+        if (msg.member.roles.cache.has("160221051983233024") || msg.member.roles.cache.has("593667878063046666")) {
         msg.channel.send(
-           new MessageEmbed()
-           .setTitle("GuardianBot (" + version + ")")
-           .setDescription("Thank you to everyone that has contributed to the creation of GuardianBot!") //its v12 thatswhy
-            .setColor(0x0000ff)
-            .addFields([
-                {
-                    name: "Authors of GuardianBot",
-                    value: "Grid21, \nVergilPrime, \nSundafyllir"
-                },
-                {
-                    name: "Additonal help",
-                    value: "Leotomas, Yoshidog2005, MarriedGeekGuy, Johna3212"
-                }
-            ])
-            .setFooter(`Created with love | Guardian`)
-           );
+            new MessageEmbed()
+                .setTitle("GuardianBot (" + version + ")")
+                .setDescription("Thank you to everyone that has contributed to the creation of GuardianBot!")
+                .setColor(0x0000ff)
+                .addFields([
+                    {
+                        name: "Authors of GuardianBot",
+                        value: "Grid21, \nVergilPrime, \nSundafyllir"
+                    },
+                    {
+                        name: "Additonal help",
+                        value: "Leotomas, Yoshidog2005, MarriedGeekGuy, Johna3212"
+                    }
+                ])
+                .setFooter(`Created with love | Guardian`)
+            );
 
-       } else return msg.channel.send("Permissions denied")
+        } else {
+            return msg.channel.send("Permissions denied");
+        }
     }
 }
--- events/message.js
+++ events/message.js
@@ -1,6 +1,7 @@
 const PREFIX = "!"
 
-const { Client, Message } = requrie("discord.js"); 
+const { Client, Message } = require("discord.js"); 
+
 const replies = require("../JSONFiles/messages.json");
 const profanities = require("profanities");
 
@@ -11,38 +12,38 @@ module.exports = {
      * @param {Message} message
      */
     run: async (client, message) => {
-        for (p in profanities) {
-            if (message.content.toLowerCase() == profanities[p].toLowerCase() || message.content.toUpperCase() == profanities[p].toUpperCase()){
-                message.channel.send('You can\'t say that. Please don\'t break the rules.');
+
+        for (let p in profanities) {
+            if (message.content.toLowerCase() === profanities[p].toLowerCase()) {
                 message.delete();
-        }
-    };
-
-
-
-
-        const data = await client.Guild.get(message.guild.id, 'prefix');
-        if (data) client.prefix = data;
-
+                (await message.channel.send('You can\'t say that. Please don\'t break the rules.')).delete({"timeout": 3000});
+            }
+        };
         if (message.author.bot) return;
         if (!message.content.startsWith(client.prefix)) {
-            const message = replies.find(m => m.message.toLowerCase() === message.content.toLowerCase)
-            if (message) {
-                message.channel.send(message.reply);
-            };
-        }
+            // if (!message.mentions.members.first()) return;
+            // if (message.mentions.members.first().id !== "591744135522680843") return;
+            const reply = replies.find(m => m.msg.toLowerCase() === message.content.toLowerCase());
+            if (reply) {
+                console.log(reply)
+                message.channel.send(reply.reply)
+            } else console.log("not found")
+        } else {
+            const data = await client.Guild.get(message.guild.id, 'prefix');
+            if (data) client.prefix = data;
+            if (!client.prefix) return;
+            //mention string of bot
+            if (!message.content.startsWith(client.prefix) || message.author.bot) return;
+            const [commandName, ...args] = message.content.slice(client.prefix.length).split(/\s+/);
+            const command = client.commands.get(commandName.toLowerCase());
+            if (!command) return;
 
-        if (!client.prefix) return;
-        if (!message.content.startsWith(client.prefix) || message.author.bot) return;
-        const [commandName, ...args] = message.content.slice(client.prefix.length).split(/\s+/);
-        const command = client.commands.get(commandName.toLowerCase());
-        if (!command) return;
-
-        try {
-            await command.run(message, args);
-        } catch (err) {
-            console.error(err);
-            message.channel.send('There was an error while trying to run that command.');
+            try {
+                await command.run(message, args);
+            } catch (err) {
+                console.error(err);
+                message.channel.send('There was an error while trying to run that command.');
+            }
         }
     }
 };
